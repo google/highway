4a0ee26363f14f76907e1a49dfd2c603de6ccb84
Jan Wassenberg <janwas@google.com>
2021-11-09 05:28:04 -0800

    add debug GCC and 32-bit GCC to run_tests.sh
    PiperOrigin-RevId: 408584054

--- run_tests.sh
+++ run_tests.sh
@@ -7,9 +7,34 @@ cd "${MYDIR}"
 # Exit if anything fails
 set -e
 
-mkdir -p build
+echo RELEASE
+rm -rf build
+mkdir build
 cd build
 cmake .. -DHWY_WARNINGS_ARE_ERRORS:BOOL=ON
 make -j
 ctest -j
+cd ..
+rm -rf build
+
+echo DEBUG GCC
+rm -rf build_dbg
+mkdir build_dbg
+cd build_dbg
+CXX=g++ CC=gcc cmake .. -DHWY_WARNINGS_ARE_ERRORS:BOOL=ON -DCMAKE_BUILD_TYPE=Debug
+make -j
+ctest -j
+cd ..
+rm -rf build_dbg
+
+echo 32-bit GCC
+rm -rf build_32
+mkdir build_32
+cd build_32
+CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32 CXX=g++ CC=gcc cmake .. -DHWY_WARNINGS_ARE_ERRORS:BOOL=ON
+make -j
+ctest -j
+cd ..
+rm -rf build_32
+
 echo Success
