b0108ffdedd12672addfca998c6cb3878df0a6ae
Evgenii Kliuchnikov <eustas@google.com>
2022-07-07 02:05:15 -0700

    Fix WASM build
    PiperOrigin-RevId: 459457519

--- hwy/ops/wasm_128-inl.h
+++ hwy/ops/wasm_128-inl.h
@@ -3298,6 +3298,16 @@ HWY_API Vec128<uint8_t, N> U8FromU32(const Vec128<uint32_t, N> v) {
 
 // ------------------------------ Truncations
 
+template <typename From, typename To, HWY_IF_UNSIGNED(From),
+          HWY_IF_UNSIGNED(To),
+          hwy::EnableIf<(sizeof(To) < sizeof(From))>* = nullptr>
+HWY_API Vec128<To, 1> TruncateTo(Simd<To, 1, 0> /* tag */,
+                                 const Vec128<From, 1> v) {
+  const Repartition<To, DFromV<decltype(v)>> d;
+  const auto v1 = BitCast(d, v);
+  return Vec128<To, 1>{v1.raw};
+}
+
 HWY_API Vec128<uint8_t, 2> TruncateTo(Simd<uint8_t, 2, 0> /* tag */,
                                       const Vec128<uint64_t> v) {
   const Full128<uint8_t> d;
@@ -3322,26 +3332,32 @@ HWY_API Vec128<uint32_t, 2> TruncateTo(Simd<uint32_t, 2, 0> /* tag */,
   return LowerHalf(ConcatEven(d, v1, v1));
 }
 
-HWY_API Vec128<uint8_t, 4> TruncateTo(Simd<uint8_t, 4, 0> /* tag */,
-                                      const Vec128<uint32_t> v) {
+template <size_t N, hwy::EnableIf<N >= 2>* = nullptr>
+HWY_API Vec128<uint8_t, N> TruncateTo(Simd<uint8_t, N, 0> /* tag */,
+                                      const Vec128<uint32_t, N> v) {
   const Full128<uint8_t> d;
-  const auto v1 = BitCast(d, v);
+  const auto v1 = Vec128<uint8_t>{v.raw};
   const auto v2 = ConcatEven(d, v1, v1);
-  return LowerHalf(LowerHalf(ConcatEven(d, v2, v2)));
+  const auto v3 = ConcatEven(d, v2, v2);
+  return Vec128<uint8_t, N>{v3.raw};
 }
 
-HWY_API Vec128<uint16_t, 4> TruncateTo(Simd<uint16_t, 4, 0> /* tag */,
-                                       const Vec128<uint32_t> v) {
+template <size_t N, hwy::EnableIf<N >= 2>* = nullptr>
+HWY_API Vec128<uint16_t, N> TruncateTo(Simd<uint16_t, N, 0> /* tag */,
+                                       const Vec128<uint32_t, N> v) {
   const Full128<uint16_t> d;
-  const auto v1 = BitCast(d, v);
-  return LowerHalf(ConcatEven(d, v1, v1));
+  const auto v1 = Vec128<uint16_t>{v.raw};
+  const auto v2 = ConcatEven(d, v1, v1);
+  return Vec128<uint16_t, N>{v2.raw};
 }
 
-HWY_API Vec128<uint8_t, 8> TruncateTo(Simd<uint8_t, 8, 0> /* tag */,
-                                      const Vec128<uint16_t> v) {
+template <size_t N, hwy::EnableIf<N >= 2>* = nullptr>
+HWY_API Vec128<uint8_t, N> TruncateTo(Simd<uint8_t, N, 0> /* tag */,
+                                      const Vec128<uint16_t, N> v) {
   const Full128<uint8_t> d;
-  const auto v1 = BitCast(d, v);
-  return LowerHalf(ConcatEven(d, v1, v1));
+  const auto v1 = Vec128<uint8_t>{v.raw};
+  const auto v2 = ConcatEven(d, v1, v1);
+  return Vec128<uint8_t, N>{v2.raw};
 }
 
 // ------------------------------ Convert i32 <=> f32 (Round)
