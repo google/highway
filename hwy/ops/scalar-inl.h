f670ea580bb70b4113b63b9cdaa42ba9b10cd13a
Evgenii Kliuchnikov <eustas@google.com>
2022-11-18 02:03:57 -0800

    Fix gcc-8 build
    PiperOrigin-RevId: 489420997

--- hwy/ops/scalar-inl.h
+++ hwy/ops/scalar-inl.h
@@ -362,7 +362,7 @@ HWY_API Vec1<T> ShiftRight(const Vec1<T> v) {
     // signed shifts are still implementation-defined.
     using TU = hwy::MakeUnsigned<T>;
     const Sisd<TU> du;
-    const TU shifted = BitCast(du, v).raw >> kBits;
+    const TU shifted = static_cast<TU>(BitCast(du, v).raw >> kBits);
     const TU sign = BitCast(du, BroadcastSignBit(v)).raw;
     const size_t sign_shift =
         static_cast<size_t>(static_cast<int>(sizeof(TU)) * 8 - 1 - kBits);
@@ -423,7 +423,7 @@ HWY_API Vec1<T> ShiftRightSame(const Vec1<T> v, int bits) {
     // signed shifts are still implementation-defined.
     using TU = hwy::MakeUnsigned<T>;
     const Sisd<TU> du;
-    const TU shifted = BitCast(du, v).raw >> bits;
+    const TU shifted = static_cast<TU>(BitCast(du, v).raw >> bits);
     const TU sign = BitCast(du, BroadcastSignBit(v)).raw;
     const size_t sign_shift =
         static_cast<size_t>(static_cast<int>(sizeof(TU)) * 8 - 1 - bits);
