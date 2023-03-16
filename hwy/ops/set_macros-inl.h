60241b99b4909e02aa54545d59a76b5d8bc82191
Jan Wassenberg <janwas@google.com>
2023-03-16 04:49:10 -0700

    GCC compile fix for HWY_NEON: add GCC-specific target string
    PiperOrigin-RevId: 517088047

--- hwy/ops/set_macros-inl.h
+++ hwy/ops/set_macros-inl.h
@@ -335,7 +335,11 @@
 #if HWY_TARGET == HWY_NEON_WITHOUT_AES
 // Do not define HWY_TARGET_STR (no pragma).
 #else
+#if HWY_COMPILER_GCC_ACTUAL
+#define HWY_TARGET_STR "fpu=crypto-neon-fp-armv8"
+#else  // clang
 #define HWY_TARGET_STR "+crypto"
+#endif  // HWY_COMPILER_*
 #endif  // HWY_TARGET == HWY_NEON_WITHOUT_AES
 
 #endif  // HWY_ARCH_ARM_V7
