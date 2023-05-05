3e12a3ddb7cdfe5132f91e7f073f0aea17631e5a
Jan Wassenberg <janwas@google.com>
2023-05-05 06:29:34 -0700

    add comment suggesting updates. Refs #952
    PiperOrigin-RevId: 529700814

--- hwy/detect_compiler_arch.h
+++ hwy/detect_compiler_arch.h
@@ -70,7 +70,8 @@
 // In case of Apple LLVM (whose version number is unrelated to that of LLVM) or
 // an invalid version number, deduce it from the presence of warnings.
 // Originally based on
-// https://github.com/simd-everywhere/simde/blob/47d6e603de9d04ee05cdfbc57cf282a02be1bf2a/simde/simde-detect-clang.h#L59
+// https://github.com/simd-everywhere/simde/blob/47d6e603de9d04ee05cdfbc57cf282a02be1bf2a/simde/simde-detect-clang.h#L59.
+// Please send updates below to them as well, thanks!
 #if defined(__apple_build_version__) || __clang_major__ >= 999
 #if __has_attribute(nouwtable)  // no new warnings in 16.0
 #define HWY_COMPILER_CLANG 1600
