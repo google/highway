d0d7cbc8db71ef30f179719261c22f368c4da05c
matthew <matthewkolbe@gmail.com>
2023-06-29 13:43:03 -0600

    creates unroller tests

---
+++ hwy/contrib/unroller/unroller_test.cc
@@ -0,0 +1,295 @@
+// Copyright Google LLC 2021
+//           Matthew Kolbe 2023
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <limits>;
+#include <iostream>;
+
+// clang-format off
+#undef HWY_TARGET_INCLUDE
+#define HWY_TARGET_INCLUDE "hwy/contrib/unroller/unroller_test.cc"
+#include "hwy/foreach_target.h"  // IWYU pragma: keep
+#include "hwy/highway.h"
+#include "hwy/contrib/unroller/unroller-inl.h"
+#include "hwy/tests/test_util-inl.h"
+// clang-format on
+
+#define UNUSED(x) (void)(x)
+
+HWY_BEFORE_NAMESPACE();
+namespace hwy {
+namespace HWY_NAMESPACE {
+
+template <typename T>
+T SimpleDot(const T* pa, const T* pb, size_t num) {
+  T sum = 0;
+  for (size_t i = 0; i < num; ++i) {
+    sum += pa[i] * pb[i];
+  }
+  return sum;
+}
+
+template <typename T>
+T SimpleAcc(const T* pa, size_t num) {
+  T sum = 0;
+  for (size_t i = 0; i < num; ++i) {
+    sum += pa[i];
+  }
+  return sum;
+}
+
+template <typename T>
+T SimpleMin(const T* pa, size_t num) {
+  T min = std::numeric_limits<T>::max();
+  for (size_t i = 0; i < num; ++i) {
+    if(min > pa[i])
+        min = pa[i];
+  }
+  return min;
+}
+
+template <typename T>
+struct accv : UnrollerUnit<accv<T>, T, T> {
+  using TT = hn::ScalableTag<T>;
+  inline hn::Vec<TT> Func(ptrdiff_t const& HWY_RESTRICT idx,
+                          hn::Vec<TT> const& HWY_RESTRICT x,
+                          hn::Vec<TT> const& HWY_RESTRICT y) const {
+    return x + y;
+  }
+
+  inline ptrdiff_t StoreImpl(ptrdiff_t const& HWY_RESTRICT idx,
+                             T* HWY_RESTRICT to, const hn::Vec<TT> x) const {
+    // no stores in a reducer
+    return 0;
+  }
+
+  inline ptrdiff_t MaskStoreImpl(const ptrdiff_t idx, T* HWY_RESTRICT to,
+                                 hn::Vec<TT> const& HWY_RESTRICT x,
+                                 ptrdiff_t const& HWY_RESTRICT places) const {
+    // no stores in a reducer
+    return 0;
+  }
+
+  inline ptrdiff_t ReduceImpl(hn::Vec<TT> const& HWY_RESTRICT x,
+                              T* HWY_RESTRICT to) const {
+    const hn::ScalableTag<T> d;
+    (*to) = hn::ReduceSum(d, x);
+    return 1;
+  }
+
+  inline void ReduceImpl(hn::Vec<TT> const& HWY_RESTRICT x0,
+                         hn::Vec<TT> const& HWY_RESTRICT x1,
+                         hn::Vec<TT> const& HWY_RESTRICT x2,
+                         hn::Vec<TT>& HWY_RESTRICT y) const {
+    y = y + x0 + x1 + x2;
+  }
+};
+
+template <typename T>
+struct minv : UnrollerUnit<minv<T>, T, T> {
+  using TT = hn::ScalableTag<T>;
+  TT d;
+
+  inline hn::Vec<TT> Func(ptrdiff_t const& HWY_RESTRICT idx,
+                          hn::Vec<TT> const& HWY_RESTRICT x,
+                          hn::Vec<TT> const& HWY_RESTRICT y) const {
+    return hn::Min(y, x);
+  }
+
+  inline hn::Vec<TT> YInitImpl() const {
+    return hn::Set(d, std::numeric_limits<T>::max());
+  }
+
+  inline hn::Vec<TT> MaskLoadImpl(ptrdiff_t const& HWY_RESTRICT idx,
+                                  T* HWY_RESTRICT from,
+                                  ptrdiff_t const& HWY_RESTRICT places) const {
+    const hn::ScalableTag<ptrdiff_t> di;
+    using TI = hn::TFromD<decltype(di)>;
+    auto mask = hn::FirstN(d, static_cast<size_t>(places));
+    auto maskneg = hn::Not(hn::FirstN(
+        d, static_cast<size_t>(places +
+                               static_cast<ptrdiff_t>(
+                                   UnrollerUnit<minv<T>, T, T>::UnitLanes()))));
+    if (places < 0) mask = maskneg;
+
+    auto def = YInitImpl();
+    return hn::MaskedLoadOr(def, mask, d, from + idx);
+  }
+
+  inline ptrdiff_t StoreImpl(ptrdiff_t const& HWY_RESTRICT idx,
+                             T* HWY_RESTRICT to,
+                             hn::Vec<TT> const& HWY_RESTRICT x) const {
+    // no stores in a reducer
+    return 0;
+  }
+
+  inline ptrdiff_t MaskStoreImpl(ptrdiff_t const& HWY_RESTRICT idx,
+                                 T* HWY_RESTRICT to,
+                                 hn::Vec<TT> const& HWY_RESTRICT x,
+                                 ptrdiff_t const& HWY_RESTRICT places) const {
+    // no stores in a reducer
+    return 0;
+  }
+
+  inline ptrdiff_t ReduceImpl(hn::Vec<TT> const& HWY_RESTRICT x,
+                              T* HWY_RESTRICT to) const {
+    const hn::ScalableTag<T> d;
+    auto minvect = hn::MinOfLanes(d, x);
+    (*to) = hn::ExtractLane(minvect, 0);
+    return 1;
+  }
+
+  inline void ReduceImpl(hn::Vec<TT> const& HWY_RESTRICT x0,
+                         hn::Vec<TT> const& HWY_RESTRICT x1,
+                         hn::Vec<TT> const& HWY_RESTRICT x2,
+                         hn::Vec<TT>& HWY_RESTRICT y) const {
+    auto a = hn::Min(x1, x0);
+    auto b = hn::Min(y, x2);
+    y = hn::Min(a, b);
+  }
+};
+
+template <typename T>
+struct dotv : UnrollerUnit2<dotv<T>, T, T, T> {
+  using TT = hn::ScalableTag<T>;
+
+  inline hn::Vec<TT> Func(ptrdiff_t const& HWY_RESTRICT idx,
+                          hn::Vec<TT> const& HWY_RESTRICT x0,
+                          hn::Vec<TT> const& HWY_RESTRICT x1,
+                          hn::Vec<TT> const& HWY_RESTRICT y) const {
+    return hn::MulAdd(x0, x1, y);
+  }
+
+  inline ptrdiff_t StoreImpl(ptrdiff_t const& HWY_RESTRICT idx,
+                             T* HWY_RESTRICT to,
+                             hn::Vec<TT> const& HWY_RESTRICT x) const {
+    // no stores in a reducer
+    return 0;
+  }
+
+  inline ptrdiff_t MaskStoreImpl(ptrdiff_t const& HWY_RESTRICT idx,
+                                 T* HWY_RESTRICT to,
+                                 hn::Vec<TT> const& HWY_RESTRICT x,
+                                 ptrdiff_t const& HWY_RESTRICT places) const {
+    // no stores in a reducer
+    return 0;
+  }
+
+  inline ptrdiff_t ReduceImpl(hn::Vec<TT> const& HWY_RESTRICT x, T* to) const {
+    const hn::ScalableTag<T> d;
+    (*to) = hn::ReduceSum(d, x);
+    return 1;
+  }
+
+  inline void ReduceImpl(hn::Vec<TT> const& HWY_RESTRICT x0,
+                         hn::Vec<TT> const& HWY_RESTRICT x1,
+                         hn::Vec<TT> const& HWY_RESTRICT x2,
+                         hn::Vec<TT>& HWY_RESTRICT y) const {
+    y = y + x0 + x1 + x2;
+  }
+};
+
+template <typename T>
+void SetValue(const float value, T* HWY_RESTRICT ptr) {
+  *ptr = static_cast<T>(value);
+}
+void SetValue(const float value, bfloat16_t* HWY_RESTRICT ptr) {
+  *ptr = BF16FromF32(value);
+}
+
+class TestUnroller {
+  // Computes/verifies one dot product.
+  template <class D>
+  void Test(D, size_t num, RandomState& rng) {
+    using T = TFromD<D>;
+
+    const auto random_t = [&rng]() {
+      const int32_t bits = static_cast<int32_t>(Random32(&rng)) & 1023;
+      return static_cast<float>(bits - 512) * (1.0f / 64);
+    };
+
+    AlignedFreeUniquePtr<T[]> pa = AllocateAligned<T>(num);
+    AlignedFreeUniquePtr<T[]> pb = AllocateAligned<T>(num);
+    HWY_ASSERT(pa && pb);
+    T* a = pa.get();
+    T* b = pb.get();
+    size_t i = 0;
+    for (; i < num; ++i) {
+      SetValue(random_t(), a + i);
+      SetValue(random_t(), b + i);
+    }
+    auto expected_dot = SimpleDot(a, b, num);
+    dotv<T> dotfn;
+    T dotr;
+    Unroller(dotfn, a, b, &dotr, static_cast<ptrdiff_t>(num));
+    HWY_ASSERT(dotr != 0);
+    HWY_ASSERT(std::abs(expected_dot - dotr) < 1e-7);
+
+    auto expected_min = SimpleMin(a, num);
+    minv<T> minfn;
+    T minr;
+    Unroller(minfn, a, &minr, static_cast<ptrdiff_t>(num));
+    
+    HWY_ASSERT(std::abs(expected_min - minr) < 1e-7);
+
+    auto expected_acc = SimpleAcc(a, num);
+    accv<T> accfn;
+    T accr;
+    Unroller(accfn, a, &accr, static_cast<ptrdiff_t>(num));
+    
+    HWY_ASSERT(std::abs(expected_acc - accr) < 1e-7);
+  }
+
+  // Runs tests with various lengths compatible with the given assumptions.
+  template <int kAssumptions, class D>
+  void ForeachCount(D d, RandomState& rng) {
+    const size_t N = Lanes(d);
+    const size_t counts[] = {1,
+                             3,
+                             7,
+                             16,
+                             HWY_MAX(N / 2, 1),
+                             HWY_MAX(2 * N / 3, 1),
+                             N,
+                             N + 1,
+                             4 * N / 3,
+                             3 * N,
+                             8 * N,
+                             8 * N + 2};
+  }
+
+ public:
+  template <class T, class D>
+  HWY_NOINLINE void operator()(T /*unused*/, D d) {
+    RandomState rng;
+    Test(d, 4000, rng);
+  }
+};
+
+void TestAllUnroller() { ForFloatTypes(ForPartialVectors<TestUnroller>()); }
+
+// NOLINTNEXTLINE(google-readability-namespace-comments)
+}  // namespace HWY_NAMESPACE
+}  // namespace hwy
+HWY_AFTER_NAMESPACE();
+
+#if HWY_ONCE
+
+namespace hwy {
+HWY_BEFORE_TEST(UnrollerTest);
+HWY_EXPORT_AND_TEST_P(UnrollerTest, TestAllUnroller);
+}  // namespace hwy
+
+#endif
