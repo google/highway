b491c2c3e24b3256cb1ac24e73d8b94ff35bba38
Jan Wassenberg <janwas@google.com>
2023-07-13 06:11:32 -0700

    MSVC warning fix (cast)
    PiperOrigin-RevId: 547774962

--- hwy/tests/mul_test.cc
+++ hwy/tests/mul_test.cc
@@ -331,8 +331,10 @@ struct TestMulAdd {
     HWY_ASSERT_VEC_EQ(d, expected.get(), NegMulAdd(neg_v2, v2, v1));
 
     for (size_t i = 0; i < N; ++i) {
-      expected[i] =
-          T(-T(i + 2u) * static_cast<T>(i + 2) + static_cast<T>(1 + i));
+      const T nm = static_cast<T>(-static_cast<MakeSigned<T>>(i + T{2}));
+      const T f = static_cast<T>(i + 2);
+      const T a = static_cast<T>(i + 1);
+      expected[i] = static_cast<T>(nm * f + a);
     }
     HWY_ASSERT_VEC_EQ(d, expected.get(), NegMulAdd(v2, v2, v1));
   }
