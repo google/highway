fb33996f1def357327755d0aafa8b4302e8d4912
Jan Wassenberg <janwas@google.com>
2023-10-16 09:15:37 -0700

    clang6 + lint fixes: cast + formatting
    PiperOrigin-RevId: 573831262

--- hwy/contrib/thread_pool/thread_pool_test.cc
+++ hwy/contrib/thread_pool/thread_pool_test.cc
@@ -39,7 +39,7 @@ using HWY_NAMESPACE::AdjustedReps;
 TEST(ThreadPoolTest, TestPool) {
   if (HWY_ARCH_WASM) return;  // WASM threading is unreliable
 
-  for (int num_threads = 0; num_threads <= 6; num_threads += 3) {
+  for (size_t num_threads = 0; num_threads <= 6; num_threads += 3) {
     ThreadPool pool(num_threads);
     for (uint32_t num_tasks = 0; num_tasks < 20; ++num_tasks) {
       std::vector<size_t> mementos(num_tasks);
@@ -75,21 +75,22 @@ TEST(ThreadPoolTest, TestSmallAssignments) {
     std::atomic<uint64_t> id_bits{0};
     std::atomic<size_t> num_calls{0};
 
-    EXPECT_TRUE(pool.Run(0, static_cast<uint32_t>(num_threads), ThreadPool::NoInit,
-                    [&](uint32_t /*task*/, const size_t thread) {
-                      num_calls.fetch_add(1, std::memory_order_relaxed);
+    EXPECT_TRUE(
+        pool.Run(0, static_cast<uint32_t>(num_threads), ThreadPool::NoInit,
+                 [&](uint32_t /*task*/, const size_t thread) {
+                   num_calls.fetch_add(1, std::memory_order_relaxed);
 
-                      EXPECT_LT(thread, num_threads);
-                      uint64_t bits = id_bits.load(std::memory_order_relaxed);
-                      while (!id_bits.compare_exchange_weak(
-                          bits, bits | (1ULL << thread))) {
-                      }
-                    }));
+                   EXPECT_LT(thread, num_threads);
+                   uint64_t bits = id_bits.load(std::memory_order_relaxed);
+                   while (!id_bits.compare_exchange_weak(
+                       bits, bits | (1ULL << thread))) {
+                   }
+                 }));
 
     // Correct number of tasks.
     EXPECT_EQ(num_threads, num_calls.load());
 
-    const int num_participants = PopCount(id_bits.load());
+    const size_t num_participants = PopCount(id_bits.load());
     // Can't expect equality because other workers may have woken up too late.
     EXPECT_LE(num_participants, num_threads);
   }
