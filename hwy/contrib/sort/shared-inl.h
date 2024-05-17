468b2af655b35c39a04b34b7bffeb0084be98e52
Highway <no-reply@google.com>
2024-05-17 10:22:33 -0700

    re-enable vqsort for sanitizers on arm
    PiperOrigin-RevId: 634816994

--- hwy/contrib/sort/shared-inl.h
+++ hwy/contrib/sort/shared-inl.h
@@ -131,9 +131,7 @@ static_assert(SortConstants::MaxBufBytes<2>(64) <= 1664, "Unexpectedly high");
 #if (HWY_TARGET == HWY_SCALAR) ||                                   \
     (HWY_COMPILER_MSVC && !HWY_IS_DEBUG_BUILD) ||                   \
     (HWY_ARCH_ARM_V7 && HWY_IS_DEBUG_BUILD) ||                      \
-    (HWY_ARCH_ARM_A64 && HWY_COMPILER_GCC_ACTUAL && HWY_IS_ASAN) || \
-    (HWY_ARCH_ARM_A64 && HWY_COMPILER_CLANG &&                      \
-     (HWY_IS_HWASAN || HWY_IS_MSAN || HWY_IS_TSAN || HWY_IS_ASAN))
+    (HWY_ARCH_ARM_A64 && HWY_COMPILER_GCC_ACTUAL && HWY_IS_ASAN)
 #define VQSORT_ENABLED 0
 #else
 #define VQSORT_ENABLED 1
