4ff77254e658ae1dd8244145d244f2f93abcc786
Jan Wassenberg <janwas@google.com>
2024-05-21 00:57:52 -0700

    simplify interleaved_test, fixes GCC AVX2/3
    PiperOrigin-RevId: 635710576

--- hwy/tests/interleaved_test.cc
+++ hwy/tests/interleaved_test.cc
@@ -31,45 +31,29 @@ struct TestLoadStoreInterleaved2 {
     const size_t N = Lanes(d);
 
     RandomState rng;
-    auto bytes = AllocateAligned<T>(2 * N);
-    // Interleave here, ensure vector results match scalar
-    auto expected = AllocateAligned<T>(3 * N);
-    // Ensure unaligned; 2 stored vectors, one zero vector.
-    auto actual_aligned = AllocateAligned<T>(3 * N + 1);
-    HWY_ASSERT(bytes && expected && actual_aligned);
 
+    constexpr size_t kVectors = 2;
     // Data to be interleaved
-    for (size_t i = 0; i < 2 * N; ++i) {
-      bytes[i] = ConvertScalarTo<T>(Random32(&rng) & 0xFF);
+    auto in = AllocateAligned<T>(kVectors * N);
+    // Ensure unaligned; kVectors plus one zero vector.
+    auto actual_aligned = AllocateAligned<T>((kVectors + 1) * N + 1);
+    HWY_ASSERT(in && actual_aligned);
+
+    for (size_t i = 0; i < kVectors * N; ++i) {
+      in[i] = ConvertScalarTo<T>(Random32(&rng) & 0x7F);
     }
-    const auto in0 = Load(d, &bytes[0 * N]);
-    const auto in1 = Load(d, &bytes[1 * N]);
+    const Vec<D> in0 = Load(d, &in[0 * N]);
+    const Vec<D> in1 = Load(d, &in[1 * N]);
 
     T* actual = actual_aligned.get() + 1;
+    StoreInterleaved2(in0, in1, d, actual);
+    StoreU(Zero(d), d, actual + kVectors * N);
 
-    for (size_t rep = 0; rep < 100; ++rep) {
-      for (size_t i = 0; i < N; ++i) {
-        expected[2 * i + 0] = bytes[0 * N + i];
-        expected[2 * i + 1] = bytes[1 * N + i];
-        // Ensure we do not write more than 2*N bytes.
-        expected[2 * N + i] = actual[2 * N + i] = 0;
-      }
-      StoreInterleaved2(in0, in1, d, actual);
-      size_t pos = 0;
-      if (!BytesEqual(expected.get(), actual, 3 * N * sizeof(T), &pos)) {
-        Print(d, "in0", in0, 0, N);
-        Print(d, "in1", in1, 0, N);
-        Print(d, "stored0", LoadU(d, actual + 0), 0, N);
-        Print(d, "stored1", LoadU(d, actual + N), 0, N);
-        fprintf(stderr, "Mismatch at pos %d\n", static_cast<int>(pos));
-        HWY_ASSERT(false);
-      }
-
-      Vec<D> out0, out1;
-      LoadInterleaved2(d, actual, out0, out1);
-      HWY_ASSERT_VEC_EQ(d, in0, out0);
-      HWY_ASSERT_VEC_EQ(d, in1, out1);
-    }
+    Vec<D> out0, out1;
+    LoadInterleaved2(d, actual, out0, out1);
+    HWY_ASSERT_VEC_EQ(d, in0, out0);
+    HWY_ASSERT_VEC_EQ(d, in1, out1);
+    HWY_ASSERT_VEC_EQ(d, Zero(d), LoadU(d, actual + kVectors * N));
   }
 };
 
@@ -77,11 +61,10 @@ HWY_NOINLINE void TestAllLoadStoreInterleaved2() {
   ForAllTypes(ForMaxPow2<TestLoadStoreInterleaved2>());
 }
 
-// Workaround for build timeout on GCC 12 aarch64, see #776; also incorrect
-// codegen for AVX3 f64.
+// Workaround for build timeout on GCC 12 aarch64, see #776.
 #undef HWY_BROKEN_LOAD34
-#if HWY_COMPILER_GCC_ACTUAL && HWY_COMPILER_GCC_ACTUAL < 1300 && \
-    (HWY_ARCH_ARM_A64 || HWY_TARGET <= HWY_AVX3)
+#if HWY_ARCH_ARM_A64 && HWY_COMPILER_GCC_ACTUAL && \
+    HWY_COMPILER_GCC_ACTUAL < 1300
 #define HWY_BROKEN_LOAD34 1
 #else
 #define HWY_BROKEN_LOAD34 0
@@ -96,50 +79,31 @@ struct TestLoadStoreInterleaved3 {
     const size_t N = Lanes(d);
 
     RandomState rng;
-    auto bytes = AllocateAligned<T>(3 * N);
-    // Interleave here, ensure vector results match scalar
-    auto expected = AllocateAligned<T>(4 * N);
-    // Ensure unaligned; 3 stored vectors, one zero vector.
-    auto actual_aligned = AllocateAligned<T>(4 * N + 1);
-    HWY_ASSERT(bytes && expected && actual_aligned);
 
+    constexpr size_t kVectors = 3;
     // Data to be interleaved
-    for (size_t i = 0; i < 3 * N; ++i) {
-      bytes[i] = ConvertScalarTo<T>(Random32(&rng) & 0xFF);
+    auto in = AllocateAligned<T>(kVectors * N);
+    // Ensure unaligned; kVectors plus one zero vector.
+    auto actual_aligned = AllocateAligned<T>((kVectors + 1) * N + 1);
+    HWY_ASSERT(in && actual_aligned);
+
+    for (size_t i = 0; i < kVectors * N; ++i) {
+      in[i] = ConvertScalarTo<T>(Random32(&rng) & 0x7F);
     }
-    const auto in0 = Load(d, &bytes[0 * N]);
-    const auto in1 = Load(d, &bytes[1 * N]);
-    const auto in2 = Load(d, &bytes[2 * N]);
+    const Vec<D> in0 = Load(d, &in[0 * N]);
+    const Vec<D> in1 = Load(d, &in[1 * N]);
+    const Vec<D> in2 = Load(d, &in[2 * N]);
 
     T* actual = actual_aligned.get() + 1;
+    StoreInterleaved3(in0, in1, in2, d, actual);
+    StoreU(Zero(d), d, actual + kVectors * N);
 
-    for (size_t rep = 0; rep < 100; ++rep) {
-      for (size_t i = 0; i < N; ++i) {
-        expected[3 * i + 0] = bytes[0 * N + i];
-        expected[3 * i + 1] = bytes[1 * N + i];
-        expected[3 * i + 2] = bytes[2 * N + i];
-        // Ensure we do not write more than 3*N bytes.
-        expected[3 * N + i] = actual[3 * N + i] = 0;
-      }
-      StoreInterleaved3(in0, in1, in2, d, actual);
-      size_t pos = 0;
-      if (!BytesEqual(expected.get(), actual, 4 * N * sizeof(T), &pos)) {
-        Print(d, "in0", in0, 0, N);
-        Print(d, "in1", in1, 0, N);
-        Print(d, "in2", in2, 0, N);
-        Print(d, "stored0", LoadU(d, actual + 0 * N), 0, N);
-        Print(d, "stored1", LoadU(d, actual + 1 * N), 0, N);
-        Print(d, "stored2", LoadU(d, actual + 2 * N), 0, N);
-        fprintf(stderr, "Mismatch at pos %d\n", static_cast<int>(pos));
-        HWY_ASSERT(false);
-      }
-
-      Vec<D> out0, out1, out2;
-      LoadInterleaved3(d, actual, out0, out1, out2);
-      HWY_ASSERT_VEC_EQ(d, in0, out0);
-      HWY_ASSERT_VEC_EQ(d, in1, out1);
-      HWY_ASSERT_VEC_EQ(d, in2, out2);
-    }
+    Vec<D> out0, out1, out2;
+    LoadInterleaved3(d, actual, out0, out1, out2);
+    HWY_ASSERT_VEC_EQ(d, in0, out0);
+    HWY_ASSERT_VEC_EQ(d, in1, out1);
+    HWY_ASSERT_VEC_EQ(d, in2, out2);
+    HWY_ASSERT_VEC_EQ(d, Zero(d), LoadU(d, actual + kVectors * N));
 #endif  // HWY_BROKEN_LOAD34
   }
 };
@@ -158,56 +122,32 @@ struct TestLoadStoreInterleaved4 {
 
     RandomState rng;
 
+    constexpr size_t kVectors = 4;
     // Data to be interleaved
-    auto in = AllocateAligned<T>(4 * N);
-    // Interleave here, ensure vector results match scalar
-    auto expected = AllocateAligned<T>(5 * N);
-    // Ensure unaligned; 4 stored vectors, one zero vector.
-    auto actual_aligned = AllocateAligned<T>(5 * N + 1);
-    HWY_ASSERT(in && expected && actual_aligned);
+    auto in = AllocateAligned<T>(kVectors * N);
+    // Ensure unaligned; kVectors plus one zero vector.
+    auto actual_aligned = AllocateAligned<T>((kVectors + 1) * N + 1);
+    HWY_ASSERT(in && actual_aligned);
 
-    for (size_t i = 0; i < 4 * N; ++i) {
+    for (size_t i = 0; i < kVectors * N; ++i) {
       in[i] = ConvertScalarTo<T>(Random32(&rng) & 0x7F);
     }
-    const auto in0 = Load(d, &in[0 * N]);
-    const auto in1 = Load(d, &in[1 * N]);
-    const auto in2 = Load(d, &in[2 * N]);
-    const auto in3 = Load(d, &in[3 * N]);
+    const Vec<D> in0 = Load(d, &in[0 * N]);
+    const Vec<D> in1 = Load(d, &in[1 * N]);
+    const Vec<D> in2 = Load(d, &in[2 * N]);
+    const Vec<D> in3 = Load(d, &in[3 * N]);
 
     T* actual = actual_aligned.get() + 1;
+    StoreInterleaved4(in0, in1, in2, in3, d, actual);
+    StoreU(Zero(d), d, actual + kVectors * N);
 
-    for (size_t rep = 0; rep < 100; ++rep) {
-      for (size_t i = 0; i < N; ++i) {
-        expected[4 * i + 0] = in[0 * N + i];
-        expected[4 * i + 1] = in[1 * N + i];
-        expected[4 * i + 2] = in[2 * N + i];
-        expected[4 * i + 3] = in[3 * N + i];
-      }
-      // Ensure we do not write more than 4*N bytes.
-      hwy::ZeroBytes(expected.get() + 4 * N, N * sizeof(T));
-      hwy::ZeroBytes(actual + 4 * N, N * sizeof(T));
-      StoreInterleaved4(in0, in1, in2, in3, d, actual);
-      size_t pos = 0;
-      if (!BytesEqual(expected.get(), actual, 5 * N * sizeof(T), &pos)) {
-        Print(d, "in0", in0, 0, N);
-        Print(d, "in1", in1, 0, N);
-        Print(d, "in2", in2, 0, N);
-        Print(d, "in3", in3, 0, N);
-        Print(d, "stored0", LoadU(d, actual + 0 * N), 0, N);
-        Print(d, "stored1", LoadU(d, actual + 1 * N), 0, N);
-        Print(d, "stored2", LoadU(d, actual + 2 * N), 0, N);
-        Print(d, "stored3", LoadU(d, actual + 3 * N), 0, N);
-        fprintf(stderr, "Mismatch at pos %d\n", static_cast<int>(pos));
-        HWY_ASSERT(false);
-      }
-
-      Vec<D> out0, out1, out2, out3;
-      LoadInterleaved4(d, actual, out0, out1, out2, out3);
-      HWY_ASSERT_VEC_EQ(d, in0, out0);
-      HWY_ASSERT_VEC_EQ(d, in1, out1);
-      HWY_ASSERT_VEC_EQ(d, in2, out2);
-      HWY_ASSERT_VEC_EQ(d, in3, out3);
-    }
+    Vec<D> out0, out1, out2, out3;
+    LoadInterleaved4(d, actual, out0, out1, out2, out3);
+    HWY_ASSERT_VEC_EQ(d, in0, out0);
+    HWY_ASSERT_VEC_EQ(d, in1, out1);
+    HWY_ASSERT_VEC_EQ(d, in2, out2);
+    HWY_ASSERT_VEC_EQ(d, in3, out3);
+    HWY_ASSERT_VEC_EQ(d, Zero(d), LoadU(d, actual + kVectors * N));
 #endif  // HWY_BROKEN_LOAD34
   }
 };
