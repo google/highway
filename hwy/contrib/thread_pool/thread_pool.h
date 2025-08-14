c00492256d71712d03e1161ff83889fb52210e32
Jan Wassenberg <janwas@google.com>
2025-08-14 09:08:41 -0700

    further simplify FreeBSD SetThreadName, refs #2670
    PiperOrigin-RevId: 795066734

--- hwy/contrib/thread_pool/thread_pool.h
+++ hwy/contrib/thread_pool/thread_pool.h
@@ -30,11 +30,6 @@
 #include <thread>  // NOLINT
 #include <vector>
 
-#include "hwy/detect_compiler_arch.h"
-#if HWY_OS_FREEBSD
-#include <pthread_np.h>
-#endif
-
 #include "hwy/aligned_allocator.h"  // HWY_ALIGNMENT
 #include "hwy/auto_tune.h"
 #include "hwy/base.h"
@@ -60,10 +55,9 @@ static inline void SetThreadName(const char* format, int thread) {
   HWY_ASSERT(0 < chars_written &&
              chars_written <= static_cast<int>(sizeof(buf) - 1));
 
-#if HWY_OS_LINUX && (!defined(__ANDROID__) || __ANDROID_API__ >= 19)
-  HWY_ASSERT(0 == pthread_setname_np(pthread_self(), buf));
-#elif HWY_OS_FREEBSD
-  // Note that pthread_set_name_np does not return a value (#2669).
+#if (HWY_OS_LINUX && (!defined(__ANDROID__) || __ANDROID_API__ >= 19)) || \
+    HWY_OS_FREEBSD
+  // Note that FreeBSD pthread_set_name_np does not return a value (#2669).
   HWY_ASSERT(0 == pthread_setname_np(pthread_self(), buf));
 #elif HWY_OS_APPLE
   // Different interface: single argument, current thread only.
