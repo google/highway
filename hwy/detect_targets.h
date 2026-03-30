8b2cf5395aa8c3a518d947c8d59fbb4d9940dfe5
Jan Wassenberg <janwas@google.com>
2026-03-30 08:21:57 -0700

    Re-enable SVE targets. Fixes #2908
    PiperOrigin-RevId: 891730025

--- hwy/detect_targets.h
+++ hwy/detect_targets.h
@@ -270,10 +270,9 @@
 // SVE[2] require recent clang or gcc versions.
 
 #ifndef HWY_BROKEN_SVE  // allow override
-// GCC 10+. Clang 22 still has test failures for SVE, including MSAN because
-// initialization info appears not to be propagated through SVE instructions.
+// Clang 22+, GCC 10+, except MSAN does not yet support SVE.
 // No Apple CPU (at least up to and including M4 and A18) has SVE.
-#if (HWY_COMPILER_CLANG && HWY_COMPILER_CLANG < 2300) ||           \
+#if (HWY_COMPILER_CLANG && HWY_COMPILER_CLANG < 2200) ||           \
     (HWY_COMPILER_GCC_ACTUAL && HWY_COMPILER_GCC_ACTUAL < 1000) || \
     HWY_OS_APPLE || HWY_IS_MSAN
 #define HWY_BROKEN_SVE (HWY_SVE | HWY_SVE_256)
@@ -283,9 +282,9 @@
 #endif  // HWY_BROKEN_SVE
 
 #ifndef HWY_BROKEN_SVE2  // allow override
-// Clang 21 still has test failures for SVE2, including MSAN because
-// initialization info appears not to be propagated through SVE instructions.
-#if (HWY_COMPILER_CLANG && HWY_COMPILER_CLANG < 2300) ||           \
+// Clang 22+, GCC 10+, except MSAN does not yet support SVE2.
+// No Apple CPU (at least up to and including M4 and A18) has SVE2.
+#if (HWY_COMPILER_CLANG && HWY_COMPILER_CLANG < 2200) ||           \
     (HWY_COMPILER_GCC_ACTUAL && HWY_COMPILER_GCC_ACTUAL < 1000) || \
     HWY_OS_APPLE || HWY_IS_MSAN
 #define HWY_BROKEN_SVE2 (HWY_SVE2)
