54ef9d73d6574d2f016b5a1c412ec19c83fa1c6e
Ravi <trivediravi561@gmail.com>
2026-04-10 07:11:02 -0400

    update CountIf

--- hwy/contrib/algo/count-inl.h
+++ hwy/contrib/algo/count-inl.h
@@ -34,13 +34,15 @@ size_t Count(D d, T value, const T* HWY_RESTRICT in, size_t count) {
   const size_t N = Lanes(d);
   const Vec<D> broadcasted = Set(d, value);
   const RebindToSigned<D> di;
+  using TI = TFromD<decltype(di)>;
   using VI = Vec<decltype(di)>;
-  const auto k1 = Set(di, TFromD<decltype(di)>(1));
 
   size_t total = 0;
   size_t i = 0;
 
+  // Min 4 lanes needed for two pairwise widenings, 8->16->32
   if constexpr (sizeof(T) == 1 && HWY_MAX_LANES_D(D) >= 4) {
+    const auto k1 = Set(di, TI{1});
     const RebindToUnsigned<decltype(di)> du;
     const RepartitionToWide<decltype(di)> di16;
     const Repartition<int32_t, D> di32;
@@ -100,6 +102,8 @@ size_t Count(D d, T value, const T* HWY_RESTRICT in, size_t count) {
     }
     total += static_cast<size_t>(ReduceSum(di32, wide_sum));
   } else if constexpr (sizeof(T) == 2 && HWY_MAX_LANES_D(D) >= 2) {
+    // Min 2 lanes needed for pairwise widening, 16->32
+    const auto k1 = Set(di, TI{1});
     const Repartition<int32_t, D> di32;
     auto wide_sum = Zero(di32);
 
@@ -140,7 +144,7 @@ size_t Count(D d, T value, const T* HWY_RESTRICT in, size_t count) {
             acc3 = Add(acc3, BitCast(di, v3));
           }
         }
-        const auto mul = HWY_NATIVE_MASK ? k1 : Neg(k1);
+        const auto mul = Set(di, HWY_NATIVE_MASK ? TI{1} : TI{-1});
         wide_sum = SatWidenMulPairwiseAccumulate(di32, acc0, mul, wide_sum);
         wide_sum = SatWidenMulPairwiseAccumulate(di32, acc1, mul, wide_sum);
         wide_sum = SatWidenMulPairwiseAccumulate(di32, acc2, mul, wide_sum);
@@ -150,6 +154,7 @@ size_t Count(D d, T value, const T* HWY_RESTRICT in, size_t count) {
     total += static_cast<size_t>(ReduceSum(di32, wide_sum));
   } else {
     // Lane type wide enough to accumulate directly
+    const auto k1 = Set(di, TI{1});
     if (count >= 4 * N) {
       VI acc0 = Zero(di);
       VI acc1 = Zero(di);
@@ -210,15 +215,148 @@ size_t Count(D d, T value, const T* HWY_RESTRICT in, size_t count) {
 template <class D, class Func, typename T = TFromD<D>>
 size_t CountIf(D d, const T* HWY_RESTRICT in, size_t count, const Func& func) {
   const size_t N = Lanes(d);
+  const RebindToSigned<D> di;
+  using TI = TFromD<decltype(di)>;
+  using VI = Vec<decltype(di)>;
 
   size_t total = 0;
   size_t i = 0;
-  if (count >= 4 * N) {
-    for (; i <= count - 4 * N; i += 4 * N) {
-      total += CountTrue(d, func(d, LoadU(d, in + i)));
-      total += CountTrue(d, func(d, LoadU(d, in + i + N)));
-      total += CountTrue(d, func(d, LoadU(d, in + i + 2 * N)));
-      total += CountTrue(d, func(d, LoadU(d, in + i + 3 * N)));
+
+  // Min 4 lanes needed for two pairwise widenings, 8->16->32
+  if constexpr (sizeof(T) == 1 && HWY_MAX_LANES_D(D) >= 4) {
+    const auto k1 = Set(di, TI{1});
+    const RebindToUnsigned<decltype(di)> du;
+    const RepartitionToWide<decltype(di)> di16;
+    const Repartition<int32_t, D> di32;
+    auto wide_sum = Zero(di32);
+
+    if (count >= 4 * N) {
+      while (i <= count - 4 * N) {
+        VI acc0 = Zero(di);
+        VI acc1 = Zero(di);
+        VI acc2 = Zero(di);
+        VI acc3 = Zero(di);
+        const size_t cap = HWY_MIN(i + 128 * 4 * N, count);
+
+        if constexpr (HWY_NATIVE_MASK) {
+          for (; i <= cap - 4 * N; i += 4 * N) {
+            const auto m0 = RebindMask(di, func(d, LoadU(d, in + i)));
+            const auto m1 = RebindMask(di, func(d, LoadU(d, in + i + N)));
+            const auto m2 = RebindMask(di, func(d, LoadU(d, in + i + 2 * N)));
+            const auto m3 = RebindMask(di, func(d, LoadU(d, in + i + 3 * N)));
+            acc0 = MaskedAddOr(acc0, m0, acc0, k1);
+            acc1 = MaskedAddOr(acc1, m1, acc1, k1);
+            acc2 = MaskedAddOr(acc2, m2, acc2, k1);
+            acc3 = MaskedAddOr(acc3, m3, acc3, k1);
+          }
+        } else {
+          for (; i <= cap - 4 * N; i += 4 * N) {
+            const auto v0 = VecFromMask(d, func(d, LoadU(d, in + i)));
+            const auto v1 = VecFromMask(d, func(d, LoadU(d, in + i + N)));
+            const auto v2 = VecFromMask(d, func(d, LoadU(d, in + i + 2 * N)));
+            const auto v3 = VecFromMask(d, func(d, LoadU(d, in + i + 3 * N)));
+            acc0 = Add(acc0, BitCast(di, v0));
+            acc1 = Add(acc1, BitCast(di, v1));
+            acc2 = Add(acc2, BitCast(di, v2));
+            acc3 = Add(acc3, BitCast(di, v3));
+          }
+
+          acc0 = Neg(acc0);
+          acc1 = Neg(acc1);
+          acc2 = Neg(acc2);
+          acc3 = Neg(acc3);
+        }
+
+        const auto w0 = SatWidenMulPairwiseAdd(di16, BitCast(du, acc0), k1);
+        const auto w1 = SatWidenMulPairwiseAdd(di16, BitCast(du, acc1), k1);
+        const auto w2 = SatWidenMulPairwiseAdd(di16, BitCast(du, acc2), k1);
+        const auto w3 = SatWidenMulPairwiseAdd(di16, BitCast(du, acc3), k1);
+        const auto sum16 = Add(Add(w0, w1), Add(w2, w3));
+        wide_sum = SatWidenMulPairwiseAccumulate(
+            di32, sum16, Set(di16, int16_t(1)), wide_sum);
+      }
+    }
+    total += static_cast<size_t>(ReduceSum(di32, wide_sum));
+  } else if constexpr (sizeof(T) == 2 && HWY_MAX_LANES_D(D) >= 2) {
+    // Min 2 lanes needed for pairwise widening, 16->32
+    const auto k1 = Set(di, TI{1});
+    const Repartition<int32_t, D> di32;
+    auto wide_sum = Zero(di32);
+
+    if (count >= 4 * N) {
+      while (i <= count - 4 * N) {
+        VI acc0 = Zero(di);
+        VI acc1 = Zero(di);
+        VI acc2 = Zero(di);
+        VI acc3 = Zero(di);
+        const size_t cap = HWY_MIN(i + 32768 * 4 * N, count);
+
+        if constexpr (HWY_NATIVE_MASK) {
+          for (; i <= cap - 4 * N; i += 4 * N) {
+            const auto m0 = RebindMask(di, func(d, LoadU(d, in + i)));
+            const auto m1 = RebindMask(di, func(d, LoadU(d, in + i + N)));
+            const auto m2 = RebindMask(di, func(d, LoadU(d, in + i + 2 * N)));
+            const auto m3 = RebindMask(di, func(d, LoadU(d, in + i + 3 * N)));
+            acc0 = MaskedAddOr(acc0, m0, acc0, k1);
+            acc1 = MaskedAddOr(acc1, m1, acc1, k1);
+            acc2 = MaskedAddOr(acc2, m2, acc2, k1);
+            acc3 = MaskedAddOr(acc3, m3, acc3, k1);
+          }
+        } else {
+          for (; i <= cap - 4 * N; i += 4 * N) {
+            const auto v0 = VecFromMask(d, func(d, LoadU(d, in + i)));
+            const auto v1 = VecFromMask(d, func(d, LoadU(d, in + i + N)));
+            const auto v2 = VecFromMask(d, func(d, LoadU(d, in + i + 2 * N)));
+            const auto v3 = VecFromMask(d, func(d, LoadU(d, in + i + 3 * N)));
+            acc0 = Add(acc0, BitCast(di, v0));
+            acc1 = Add(acc1, BitCast(di, v1));
+            acc2 = Add(acc2, BitCast(di, v2));
+            acc3 = Add(acc3, BitCast(di, v3));
+          }
+        }
+        const auto mul = Set(di, HWY_NATIVE_MASK ? TI{1} : TI{-1});
+        wide_sum = SatWidenMulPairwiseAccumulate(di32, acc0, mul, wide_sum);
+        wide_sum = SatWidenMulPairwiseAccumulate(di32, acc1, mul, wide_sum);
+        wide_sum = SatWidenMulPairwiseAccumulate(di32, acc2, mul, wide_sum);
+        wide_sum = SatWidenMulPairwiseAccumulate(di32, acc3, mul, wide_sum);
+      }
+    }
+    total += static_cast<size_t>(ReduceSum(di32, wide_sum));
+  } else {
+    // Lane type wide enough to accumulate directly
+    const auto k1 = Set(di, TI{1});
+    if (count >= 4 * N) {
+      VI acc0 = Zero(di);
+      VI acc1 = Zero(di);
+      VI acc2 = Zero(di);
+      VI acc3 = Zero(di);
+
+      if constexpr (HWY_NATIVE_MASK) {
+        for (; i <= count - 4 * N; i += 4 * N) {
+          const auto m0 = RebindMask(di, func(d, LoadU(d, in + i)));
+          const auto m1 = RebindMask(di, func(d, LoadU(d, in + i + N)));
+          const auto m2 = RebindMask(di, func(d, LoadU(d, in + i + 2 * N)));
+          const auto m3 = RebindMask(di, func(d, LoadU(d, in + i + 3 * N)));
+          acc0 = MaskedAddOr(acc0, m0, acc0, k1);
+          acc1 = MaskedAddOr(acc1, m1, acc1, k1);
+          acc2 = MaskedAddOr(acc2, m2, acc2, k1);
+          acc3 = MaskedAddOr(acc3, m3, acc3, k1);
+        }
+        acc0 = Add(Add(acc0, acc1), Add(acc2, acc3));
+      } else {
+        for (; i <= count - 4 * N; i += 4 * N) {
+          const auto v0 = VecFromMask(d, func(d, LoadU(d, in + i)));
+          const auto v1 = VecFromMask(d, func(d, LoadU(d, in + i + N)));
+          const auto v2 = VecFromMask(d, func(d, LoadU(d, in + i + 2 * N)));
+          const auto v3 = VecFromMask(d, func(d, LoadU(d, in + i + 3 * N)));
+          acc0 = Add(acc0, BitCast(di, v0));
+          acc1 = Add(acc1, BitCast(di, v1));
+          acc2 = Add(acc2, BitCast(di, v2));
+          acc3 = Add(acc3, BitCast(di, v3));
+        }
+        acc0 = Neg(Add(Add(acc0, acc1), Add(acc2, acc3)));
+      }
+      total += static_cast<size_t>(ReduceSum(di, acc0));
     }
   }
 
@@ -229,20 +367,10 @@ size_t CountIf(D d, const T* HWY_RESTRICT in, size_t count, const Func& func) {
   }
 
   if (i != count) {
-#if HWY_MEM_OPS_MIGHT_FAULT
-    const CappedTag<T, 1> d1;
-    for (; i < count; ++i) {
-      if (AllTrue(d1, func(d1, LoadU(d1, in + i)))) {
-        total += 1;
-      }
-    }
-#else
     const size_t remaining = count - i;
     HWY_DASSERT(0 != remaining && remaining < N);
-    const Mask<D> mask = FirstN(d, remaining);
-    const Vec<D> v = MaskedLoad(mask, d, in + i);
-    total += CountTrue(d, And(func(d, v), mask));
-#endif  // HWY_MEM_OPS_MIGHT_FAULT
+    const Vec<D> v = LoadN(d, in + i, remaining);
+    total += CountTrue(d, And(func(d, v), FirstN(d, remaining)));
   }
 
   return total;
