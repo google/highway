c430a42dd208747e64c695c8522cb8734192ed87
Jan Wassenberg <janwas@google.com>
2026-06-03 11:05:30 -0700

    faster Find: unroll 2x, masking.
    PiperOrigin-RevId: 926147599

--- hwy/contrib/algo/find-inl.h
+++ hwy/contrib/algo/find-inl.h
@@ -22,6 +22,9 @@
 #define HIGHWAY_HWY_CONTRIB_ALGO_FIND_INL_H_
 #endif
 
+#include <stddef.h>
+#include <stdint.h>
+
 #include "hwy/highway.h"
 
 HWY_BEFORE_NAMESPACE();
@@ -32,37 +35,36 @@ namespace HWY_NAMESPACE {
 // `count` if not found.
 template <class D, typename T = TFromD<D>>
 size_t Find(D d, T value, const T* HWY_RESTRICT in, size_t count) {
-  const size_t N = Lanes(d);
+  HWY_LANES_CONSTEXPR size_t N = Lanes(d);
   const Vec<D> broadcasted = Set(d, value);
 
   size_t i = 0;
-  if (count >= N) {
-    for (; i <= count - N; i += N) {
-      const intptr_t pos = FindFirstTrue(d, Eq(broadcasted, LoadU(d, in + i)));
+  if (HWY_LIKELY(count >= 2 * N)) {
+    for (; i <= count - 2 * N; i += 2 * N) {
+      const Mask<D> eq0 = Eq(broadcasted, LoadU(d, in + i + 0 * N));
+      const Mask<D> eq1 = Eq(broadcasted, LoadU(d, in + i + 1 * N));
+      if (AllFalse(d, Or(eq0, eq1))) continue;
+      const intptr_t pos = FindFirstTrue(d, eq0);
       if (pos >= 0) return i + static_cast<size_t>(pos);
+      return i + N + FindKnownFirstTrue(d, eq1);
     }
   }
 
-  if (i != count) {
-#if HWY_MEM_OPS_MIGHT_FAULT
-    // Scan single elements.
-    const CappedTag<T, 1> d1;
-    using V1 = Vec<decltype(d1)>;
-    const V1 broadcasted1 = Set(d1, GetLane(broadcasted));
-    for (; i < count; ++i) {
-      if (AllTrue(d1, Eq(broadcasted1, LoadU(d1, in + i)))) {
-        return i;
-      }
-    }
-#else
-    const size_t remaining = count - i;
-    HWY_DASSERT(0 != remaining && remaining < N);
-    const Mask<D> mask = FirstN(d, remaining);
-    const Vec<D> v = MaskedLoad(mask, d, in + i);
-    // Apply mask so that we don't 'find' the zero-padding from MaskedLoad.
-    const intptr_t pos = FindFirstTrue(d, And(Eq(broadcasted, v), mask));
+  size_t remaining = count - i;
+  if (remaining >= N) {
+    const Mask<D> eq = Eq(broadcasted, LoadU(d, in + i));
+    const intptr_t pos = FindFirstTrue(d, eq);
     if (pos >= 0) return i + static_cast<size_t>(pos);
-#endif  // HWY_MEM_OPS_MIGHT_FAULT
+    i += N;
+    remaining -= N;
+  }
+
+  HWY_DASSERT(remaining < N);
+  if (remaining != 0) {
+    const Mask<D> mask = FirstN(d, remaining);
+    const Mask<D> eq = MaskedEq(mask, broadcasted, LoadN(d, in + i, remaining));
+    const intptr_t pos = FindFirstTrue(d, eq);
+    if (pos >= 0) return i + static_cast<size_t>(pos);
   }
 
   return count;  // not found
@@ -72,34 +74,35 @@ size_t Find(D d, T value, const T* HWY_RESTRICT in, size_t count) {
 // returns true, otherwise `count`.
 template <class D, class Func, typename T = TFromD<D>>
 size_t FindIf(D d, const T* HWY_RESTRICT in, size_t count, const Func& func) {
-  const size_t N = Lanes(d);
+  HWY_LANES_CONSTEXPR size_t N = Lanes(d);
 
   size_t i = 0;
-  if (count >= N) {
-    for (; i <= count - N; i += N) {
-      const intptr_t pos = FindFirstTrue(d, func(d, LoadU(d, in + i)));
+  if (HWY_LIKELY(count >= 2 * N)) {
+    for (; i <= count - 2 * N; i += 2 * N) {
+      const Mask<D> eq0 = func(LoadU(d, in + i + 0 * N));
+      const Mask<D> eq1 = func(LoadU(d, in + i + 1 * N));
+      if (AllFalse(d, Or(eq0, eq1))) continue;
+      const intptr_t pos = FindFirstTrue(d, eq0);
       if (pos >= 0) return i + static_cast<size_t>(pos);
+      return i + N + FindKnownFirstTrue(d, eq1);
     }
   }
 
-  if (i != count) {
-#if HWY_MEM_OPS_MIGHT_FAULT
-    // Scan single elements.
-    const CappedTag<T, 1> d1;
-    for (; i < count; ++i) {
-      if (AllTrue(d1, func(d1, LoadU(d1, in + i)))) {
-        return i;
-      }
-    }
-#else
-    const size_t remaining = count - i;
-    HWY_DASSERT(0 != remaining && remaining < N);
-    const Mask<D> mask = FirstN(d, remaining);
-    const Vec<D> v = MaskedLoad(mask, d, in + i);
-    // Apply mask so that we don't 'find' the zero-padding from MaskedLoad.
-    const intptr_t pos = FindFirstTrue(d, And(func(d, v), mask));
+  size_t remaining = count - i;
+  if (remaining >= N) {
+    const Mask<D> eq = func(LoadU(d, in + i));
+    const intptr_t pos = FindFirstTrue(d, eq);
     if (pos >= 0) return i + static_cast<size_t>(pos);
-#endif  // HWY_MEM_OPS_MIGHT_FAULT
+    i += N;
+    remaining -= N;
+  }
+
+  HWY_DASSERT(remaining < N);
+  if (remaining != 0) {
+    const Mask<D> mask = FirstN(d, remaining);
+    const Mask<D> eq = And(mask, func(LoadN(d, in + i, remaining)));
+    const intptr_t pos = FindFirstTrue(d, eq);
+    if (pos >= 0) return i + static_cast<size_t>(pos);
   }
 
   return count;  // not found
