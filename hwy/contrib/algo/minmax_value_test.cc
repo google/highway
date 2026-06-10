084b82554c7801f815f066a0fdc1929e1a19dd35
Krzysztof Rymski <krymski@google.com>
2026-06-10 07:52:51 -0700

    Add implementations of Reduce2/4/8 to quickly compute simultaneous sum/min/max of N registers + reusable template for those functions.
    PiperOrigin-RevId: 929852753

--- hwy/contrib/algo/minmax_value_test.cc
+++ hwy/contrib/algo/minmax_value_test.cc
@@ -146,6 +146,101 @@ void TestAllMaxValue() {
   ForAllTypes(ForPartialVectors<ForeachCountAndMisalign<TestMaxValue>>());
 }
 
+struct TestReduce {
+  template <typename T, class D>
+  HWY_NOINLINE void operator()(T /*unused*/, D d) const {
+    const size_t N = Lanes(d);
+    AlignedFreeUniquePtr<T[]> storage = AllocateAligned<T>(8 * N);
+    HWY_ASSERT(storage);
+    T* in = storage.get();
+    for (size_t i = 0; i < 8 * N; ++i) {
+      in[i] = static_cast<T>((i % 13) + 1);
+    }
+
+    auto x0 = Load(d, in + 0 * N);
+    auto x1 = Load(d, in + 1 * N);
+    auto x2 = Load(d, in + 2 * N);
+    auto x3 = Load(d, in + 3 * N);
+    auto x4 = Load(d, in + 4 * N);
+    auto x5 = Load(d, in + 5 * N);
+    auto x6 = Load(d, in + 6 * N);
+    auto x7 = Load(d, in + 7 * N);
+
+    T min_s[8];
+    T max_s[8];
+    T sum_s[8];
+    for (int v = 0; v < 8; ++v) {
+      min_s[v] = hwy::PositiveInfOrHighestValue<T>();
+      max_s[v] = hwy::NegativeInfOrLowestValue<T>();
+      sum_s[v] = 0;
+      for (size_t i = 0; i < N; ++i) {
+        T val = in[v * N + i];
+        if (val < min_s[v]) min_s[v] = val;
+        if (val > max_s[v]) max_s[v] = val;
+        sum_s[v] = AddWithWraparound(sum_s[v], val);
+      }
+    }
+
+    using D2 = CappedTag<T, 2>;
+    const D2 d2;
+    if (Lanes(d2) == 2) {
+      HWY_ALIGN T actual_min[2];
+      HWY_ALIGN T actual_max[2];
+      HWY_ALIGN T actual_sum[2];
+      auto p_min = Reduce2Min(d, x0, x1);
+      actual_min[0] = p_min.first;
+      actual_min[1] = p_min.second;
+      auto p_max = Reduce2Max(d, x0, x1);
+      actual_max[0] = p_max.first;
+      actual_max[1] = p_max.second;
+      auto p_sum = Reduce2Sum(d, x0, x1);
+      actual_sum[0] = p_sum.first;
+      actual_sum[1] = p_sum.second;
+      for (int i = 0; i < 2; ++i) {
+        HWY_ASSERT_EQ(min_s[i], actual_min[i]);
+        HWY_ASSERT_EQ(max_s[i], actual_max[i]);
+        HWY_ASSERT_EQ(sum_s[i], actual_sum[i]);
+      }
+    }
+
+    using D4 = CappedTag<T, 4>;
+    const D4 d4;
+    if (Lanes(d4) == 4) {
+      HWY_ALIGN T actual_min[4];
+      HWY_ALIGN T actual_max[4];
+      HWY_ALIGN T actual_sum[4];
+      Store(Reduce4Min(d, x0, x1, x2, x3), d4, actual_min);
+      Store(Reduce4Max(d, x0, x1, x2, x3), d4, actual_max);
+      Store(Reduce4Sum(d, x0, x1, x2, x3), d4, actual_sum);
+      for (int i = 0; i < 4; ++i) {
+        HWY_ASSERT_EQ(min_s[i], actual_min[i]);
+        HWY_ASSERT_EQ(max_s[i], actual_max[i]);
+        HWY_ASSERT_EQ(sum_s[i], actual_sum[i]);
+      }
+    }
+
+    using D8 = CappedTag<T, 8>;
+    const D8 d8;
+    if (Lanes(d8) == 8) {
+      HWY_ALIGN T actual_min[8];
+      HWY_ALIGN T actual_max[8];
+      HWY_ALIGN T actual_sum[8];
+      Store(Reduce8Min(d, x0, x1, x2, x3, x4, x5, x6, x7), d8, actual_min);
+      Store(Reduce8Max(d, x0, x1, x2, x3, x4, x5, x6, x7), d8, actual_max);
+      Store(Reduce8Sum(d, x0, x1, x2, x3, x4, x5, x6, x7), d8, actual_sum);
+      for (int i = 0; i < 8; ++i) {
+        HWY_ASSERT_EQ(min_s[i], actual_min[i]);
+        HWY_ASSERT_EQ(max_s[i], actual_max[i]);
+        HWY_ASSERT_EQ(sum_s[i], actual_sum[i]);
+      }
+    }
+  }
+};
+
+void TestAllReduce() {
+  ForAllTypes(ForPartialVectors<TestReduce>());
+}
+
 }  // namespace
 // NOLINTNEXTLINE(google-readability-namespace-comments)
 }  // namespace HWY_NAMESPACE
@@ -158,6 +253,7 @@ namespace {
 HWY_BEFORE_TEST(MinMaxTest);
 HWY_EXPORT_AND_TEST_P(MinMaxTest, TestAllMinValue);
 HWY_EXPORT_AND_TEST_P(MinMaxTest, TestAllMaxValue);
+HWY_EXPORT_AND_TEST_P(MinMaxTest, TestAllReduce);
 HWY_AFTER_TEST();
 }  // namespace
 }  // namespace hwy
